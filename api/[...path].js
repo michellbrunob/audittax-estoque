c4abdcc5fd458f0d0f47538d0a334fe91ddb7eb4
michellbrunob <michellbrunob@gmail.com>
2026-05-04 16:33:51 -0300

    Ajusta rota API da Vercel para ESM

--- api/[...path].js
+++ api/[...path].js
@@ -1,13 +1,16 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 const app = require('../backend/server.js');
 const { initDatabase } = require('../backend/db/database.js');
 
 let initPromise = null;
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   if (!initPromise) {
     initPromise = initDatabase();
   }
 
   await initPromise;
   return app(req, res);
-};
+}
